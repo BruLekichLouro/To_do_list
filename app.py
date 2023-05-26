7ccc1fff5c37c6388f6fe9f329f3b4182ba787d9
BruLekichLouro <bububio@yahoo.com.br>
2023-05-26 09:44:54 -0300

    Criando função para adicionar tarefas e imprimir as tarefas

--- app.py
+++ app.py
@@ -9,15 +9,25 @@ MENU_OPTIONS = """
 # Variável para armazenar a LISTA de tarefas
 tasks = []
 
+def add_task():
+    task = input("Armazenar a tarefa: ")
+    tasks.append(task)
 
+def print_tasks():
+    for task in tasks:
+        print(task)
+    
 def menu():
     # Função principal do programa
 
     while True:
+        
+        print(tasks)
+        
         option = input(MENU_OPTIONS)
 
         if option == "1":
-            ...
+            add_task()
 
         elif option == "2":
             ...
